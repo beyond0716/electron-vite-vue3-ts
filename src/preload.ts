74671dc98f07f7c4f0740ee8d917b4d3462ba374
Fxx <beyond0716@foxmail.com>
2024-08-05 22:53:01 +0800

    feat: 通过预加载脚本从渲染器访问Node.js

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,15 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+// 所有的 Node.js API接口 都可以在 preload 进程中被调用.
+// 它拥有与Chrome扩展一样的沙盒。
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
