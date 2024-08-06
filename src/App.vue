04fb6758232513763fda0a516cb2af24a8ff4a3b
Fxx <beyond0716@foxmail.com>
2024-08-06 21:09:43 +0800

    feat: 集成vue3

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>💖 Hello World!</h1>
+  <p>{{ msg }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+console.log('👋 This message is being logged by "App.vue", included via Vite')
+const msg = ref('Welcome to your Electron application.')
+</script>
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,12 +1,13 @@
-import type { ConfigEnv, UserConfig } from 'vite';
-import { defineConfig } from 'vite';
-import { pluginExposeRenderer } from './vite.base.config';
+import type { ConfigEnv, UserConfig } from 'vite'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { pluginExposeRenderer } from './vite.base.config'
 
 // https://vitejs.dev/config
 export default defineConfig((env) => {
-  const forgeEnv = env as ConfigEnv<'renderer'>;
-  const { root, mode, forgeConfigSelf } = forgeEnv;
-  const name = forgeConfigSelf.name ?? '';
+  const forgeEnv = env as ConfigEnv<'renderer'>
+  const { root, mode, forgeConfigSelf } = forgeEnv
+  const name = forgeConfigSelf.name ?? ''
 
   return {
     root,
@@ -15,10 +16,10 @@ export default defineConfig((env) => {
     build: {
       outDir: `.vite/renderer/${name}`,
     },
-    plugins: [pluginExposeRenderer(name)],
+    plugins: [pluginExposeRenderer(name), vue()],
     resolve: {
       preserveSymlinks: true,
     },
     clearScreen: false,
-  } as UserConfig;
-});
+  } as UserConfig
+})
