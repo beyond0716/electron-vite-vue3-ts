30cd0075d4f565ab63eb701bad317180c0fae14c
Feng <beyond0716@foxmail.com>
2024-08-13 14:46:57 +0800

    feat: 通知

--- electron/main/main.ts
+++ electron/main/main.ts
@@ -1,5 +1,5 @@
 import { updateElectronApp } from 'update-electron-app'
-import { app, BrowserWindow, ipcMain } from 'electron'
+import { app, BrowserWindow, ipcMain, Notification } from 'electron'
 import path from 'path'
 
 updateElectronApp()
@@ -7,6 +7,16 @@ updateElectronApp()
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
   app.quit()
+}
+
+const NOTIFICATION_TITLE = 'Basic Notification'
+const NOTIFICATION_BODY = 'Notification from the Main process'
+
+function showNotification() {
+  new Notification({
+    title: NOTIFICATION_TITLE,
+    body: NOTIFICATION_BODY,
+  }).show()
 }
 
 const createWindow = () => {
@@ -29,15 +39,18 @@ const createWindow = () => {
   mainWindow.webContents.openDevTools()
 }
 
-app.whenReady().then(() => {
-  createWindow()
+app
+  .whenReady()
+  .then(() => {
+    createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+      }
+    })
   })
-})
+  .then(showNotification)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
-<template></template>
+<template>
+  <h1>Hello World!</h1>
+  <p>
+    After launching this application, you should see the system notification.
+  </p>
+  <p>{{ CLICK_MESSAGE }}</p>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const NOTIFICATION_TITLE = 'Title'
+const NOTIFICATION_BODY =
+  'Notification from the Renderer process. Click to log to console.'
+const CLICK_MESSAGE = ref('Click it to see the effect in this interface.')
+
+new window.Notification(NOTIFICATION_TITLE, {
+  body: NOTIFICATION_BODY,
+}).onclick = () => {
+  CLICK_MESSAGE.value = 'Notification clicked!'
+}
+</script>
