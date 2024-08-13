88634cfd0c45db5db5ecf7d5c68ba307ad4ff045
Feng <beyond0716@foxmail.com>
2024-08-13 15:00:49 +0800

    feat: 进度条

--- electron/main/main.ts
+++ electron/main/main.ts
@@ -9,6 +9,8 @@ if (require('electron-squirrel-startup')) {
   app.quit()
 }
 
+let progressInterval
+
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -26,6 +28,23 @@ const createWindow = () => {
     )
   }
 
+  const INCREMENT = 0.03
+  const INTERVAL_DELAY = 100 // ms
+
+  let c = 0
+  progressInterval = setInterval(() => {
+    // update progress bar to next value
+    // values between 0 and 1 will show progress, >1 will show indeterminate or stick at 100%
+    mainWindow.setProgressBar(c)
+
+    // increment or reset progress bar
+    if (c < 2) {
+      c += INCREMENT
+    } else {
+      c = -INCREMENT * 5 // reset to a bit less than 0 to show reset state
+    }
+  }, INTERVAL_DELAY)
+
   mainWindow.webContents.openDevTools()
 }
 
@@ -37,6 +56,11 @@ app.whenReady().then(() => {
       createWindow()
     }
   })
+})
+
+// before the app is terminated, clear both timers
+app.on('before-quit', () => {
+  clearInterval(progressInterval)
 })
 
 app.on('window-all-closed', () => {
